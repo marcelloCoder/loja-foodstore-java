e81df1fde250f80c3133bf7972ab49daaa3d9f24
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2022-12-18 00:39:41 -0300

    intial

---
+++ src/main/java/br/com/foodstore/LojaFoodstoreJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.foodstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaFoodstoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaFoodstoreJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/foodstore/LojaFoodstoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.foodstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaFoodstoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
