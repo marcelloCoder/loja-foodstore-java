0b07af2f51fe30ebb30e6bcad08aea1fe414d5de
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2023-01-09 14:02:13 -0300

    Classes , Enums e SQL

---
+++ src/main/java/br/com/foodstore/enums/StatusContaReceber.java
@@ -0,0 +1,26 @@
+package br.com.foodstore.enums;
+
+public enum StatusContaReceber {
+	
+	COBRANCA("Pagar"),
+	VENCIDA("Vencida"),
+	ABERTA("Aberta"),
+	QUITADA("Quitada");
+	
+	private String descricao;
+	
+	private StatusContaReceber(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.descricao;
+	}
+
+}
