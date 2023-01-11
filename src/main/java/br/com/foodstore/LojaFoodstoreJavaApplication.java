e855a67f274fac617ac9218b31c4f19b8c0700ee
Marcello Vieira Mueno <deoliveiral767@gmail.com>
2023-01-10 21:52:12 -0300

    Classes

--- src/main/java/br/com/foodstore/LojaFoodstoreJavaApplication.java
+++ src/main/java/br/com/foodstore/LojaFoodstoreJavaApplication.java
@@ -2,8 +2,10 @@ package br.com.foodstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "br.com.foodstore.model")
 public class LojaFoodstoreJavaApplication {
 
 	public static void main(String[] args) {
